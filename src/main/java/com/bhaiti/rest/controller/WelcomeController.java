b9757a94ebe80f60523a06401ec2d71a2266fbb5
mani-demo <38170649+mani-demo@users.noreply.github.com>
2018-09-15 07:20:42 +0530

    test

--- src/main/java/com/bhaiti/rest/controller/WelcomeController.java
+++ src/main/java/com/bhaiti/rest/controller/WelcomeController.java
@@ -21,7 +21,7 @@ public class WelcomeController {
 
     @GetMapping("/welcome/user")
     @ResponseBody
-    public Welcome welcomeUser(@RequestParam(name="name", required=false, defaultValue="Java Fan") String name) {
+    public Welcome welcomeUser(@RequestParam(name="name", required=false, defaultValue="CI/CD Process Done") String name) {
         return new Welcome(String.format(welcomemsg, name));
     }
 
